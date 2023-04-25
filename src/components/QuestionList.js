327ec895ef0c8c80f047b143e58cc2f37599512f
Njalale <fnjalale@gmail.com>
2023-04-25 21:39:27 +0300

    Question List

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function QuestionList() {
+  const [question, setQuestion] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then(resp => resp.json())
+    .then(data => setQuestion(data))
+  },[])
+
+  const allQuestions = question.map(quest => {
+    return(
+      <li key={quest.id}> {quest.prompt} </li>
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{allQuestions}</ul>
     </section>
   );
 }
